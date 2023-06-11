61c5fc40e192a39eac0a6dcda92810925e79c79c
Enrique Villalobos Andrade <enrique.lnx@gmail.com>
2023-06-10 20:08:29 -0400

    Auto-generated commit [ci skip]

--- src/app/pipes/truncate-description.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TruncateDescriptionPipe } from './truncate-description.pipe';
-
-describe('TruncateDescriptionPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TruncateDescriptionPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
